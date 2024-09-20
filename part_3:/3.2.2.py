c24c6defbc921808169875f4ab770b39d454920f
Oleksandr Svirskiy <oleksandr.svirskiy@gmail.com>
2024-09-20 16:01:01 +0200

    3.2

---
+++ part_3:/3.2.2.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+sum_1 = (a + b) ** 2
+sum_2 = a ** 2 + b ** 2
+print("Square of the sum", a, "и", b, "equal", sum_1)
+print("Sum of squares", a, "и", b, "equal", sum_2)
---
+++ part_3:/3.2.4.py
@@ -0,0 +1,2 @@
+n = int(input())
+print(n + (n * 11) + (n * 111))
