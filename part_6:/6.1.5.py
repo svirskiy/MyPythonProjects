1032b7694e9f84b88b9f55998e27bbec5c695139
Oleksandr Svirskiy <oleksandr.svirskiy@gmail.com>
2024-10-03 14:24:55 +0200

    6.1 after comments

--- part_6:/6.1.5.py
+++ part_6:/6.1.5.py
@@ -1,5 +1,5 @@
 dog_age = int(input())
 if dog_age <= 2:
     print(dog_age * 10.5)
-elif dog_age >= 3:
+elif dog_age > 2:
     print(((dog_age - 2) * 4) + 21)
--- part_6:/6.1.9.py
+++ part_6:/6.1.9.py
@@ -4,6 +4,5 @@ number_3 = int(input())
 minimum_number = min(number_1, number_2, number_3)
 maximum_number = max(number_1, number_2, number_3)
 middle_number = number_1 + number_2 + number_3 - maximum_number - minimum_number
-print(maximum_number)
-print(middle_number)
-print(minimum_number)
+print(maximum_number, middle_number, minimum_number, sep="\n")
+
